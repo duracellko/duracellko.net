3a9067fdac84755dc92ea91e8746f874ef24d22b
Rastislav Novotný <rasto@duracellko.net>
2024-06-25 21:57:23 +0800

    Round-lot validation part 1

--- Duracellko.NET/Program.cs
+++ Duracellko.NET/Program.cs
@@ -4,5 +4,5 @@ return await Bootstrapper.Factory
     .CreateWeb(args)
     .AddSetting(
         MarkdownKeys.MarkdownExtensions,
-        new List<string> { "EmphasisExtra", "Math" })
+        new List<string> { "Bootstrap", "EmphasisExtra", "Math" })
     .RunAsync();
