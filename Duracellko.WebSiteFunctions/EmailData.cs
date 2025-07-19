593a963b200a7993f2d0ece129929e9ce252f1c7
Rastislav Novotný <rasto@duracellko.net>
2025-07-19 09:26:15 +0800

    Send Email Azure function

---
+++ Duracellko.WebSiteFunctions/EmailData.cs
@@ -0,0 +1,15 @@
+﻿namespace Duracellko.WebSiteFunctions;
+
+/// <summary>
+/// Data included in HTTP request to compose email.
+/// </summary>
+public class EmailData
+{
+    public string? SenderEmail { get; set; }
+
+    public string? SenderName { get; set; }
+
+    public string? Subject { get; set; }
+
+    public string? Message { get; set; }
+}
